98f04953bdb85ed3629e62c87fa6b2e145fe4027
Ahmed Strange <me@rancho.me>
2023-02-22 02:59:58 +0200

    feat: adds tests skeleton that determines required features

---
+++ tests.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+class TestHumanizeTime(unittest.TestCase):
+
+    def test_time_oclock(self):
+        pass
+
+    def test_special_day_times(self):
+        pass
+
+    def test_minutes_above_30(self):
+        pass
+
+    def test_minutes_under_30(self):
+        pass
+
+    def test_minutes_exactly_30(self):
+        pass
+
+    def test_switch_between_am_to_pm(self):
+        pass
+
+    def test_switching_from_pm_to_am(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
