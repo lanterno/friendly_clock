77c0182f1bb3a6aaa431e60df07ea4d7d6104c8e
Ahmed Strange <me@rancho.me>
2023-02-22 04:14:53 +0200

    feat: adds parsing tests

--- tests.py
+++ tests.py
@@ -1,5 +1,46 @@
 import unittest
 
+from time import Time, TimeParserException
+
+
+class TestHumanizeTimeParser(unittest.TestCase):
+
+    def test_wrong_time_format(self):
+        with self.assertRaises(TimeParserException):
+            Time.from_str("aa:bb")
+
+        with self.assertRaises(TimeParserException):
+            Time.from_str("1:2")
+
+    def test_parse_special_day_times(self):
+        # midday
+        time = Time.from_str("12:00")
+        self.assertEqual(time.hour, 0)
+        self.assertEqual(time.minute, 0)
+        self.assertEqual(time.pm, True)
+        # midnight
+        time = Time.from_str("24:00")
+        self.assertEqual(time.hour, 0)
+        self.assertEqual(time.minute, 0)
+        self.assertEqual(time.pm, False)
+        # another way to represent midnight
+        time = Time.from_str("00:00")
+        self.assertEqual(time.hour, 0)
+        self.assertEqual(time.minute, 0)
+        self.assertEqual(time.pm, False)
+
+    def test_parse_normal_times(self):
+        # AM
+        time = Time.from_str("04:12")
+        self.assertEqual(time.hour, 4)
+        self.assertEqual(time.minute, 12)
+        self.assertEqual(time.pm, False)
+        # PM
+        time = Time.from_str("14:35")
+        self.assertEqual(time.hour, 2)
+        self.assertEqual(time.minute, 35)
+        self.assertEqual(time.pm, True)
+
 
 class TestHumanizeTime(unittest.TestCase):
 
